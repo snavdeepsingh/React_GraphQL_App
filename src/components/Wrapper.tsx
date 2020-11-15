535a31e70e10aa1f15cea22c6ddb8abbc995cc6b
nsinghROLO <nsingh@rodeologistics.co>
2020-11-15 14:50:14 -0600

    Added InputField, Wrapper components. Added register page. Yarn add urql and graphql.

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,19 @@
+import { Box } from '@chakra-ui/core';
+import React from 'react'
+
+interface WrapperProps {
+  variant?: 'small' | 'regular'
+}
+
+export const Wrapper: React.FC<WrapperProps> = ({children, variant = 'regular'}) => {
+  return (
+    <Box
+      maxWidth={ variant === 'regular' ? '800px' : '400px'}
+      mt={8}
+      mx="auto"
+      w="100%"
+    >
+      {children}
+    </Box>
+  );
+}
---
+++ src/pages/register.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Formik, Form } from 'formik';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+import { Box, Button } from '@chakra-ui/core';
+
+interface registerProps {
+
+}
+
+
+
+const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant='small'>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        {({isSubmitting}) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              type="submit"
+              mt={4}
+              colorScheme="teal"
+              isLoading={isSubmitting}
+            >register</Button>
+          </Form>
+        )}
+        </Formik>
+      </Wrapper>
+    );
+}
+
+export default Register;
